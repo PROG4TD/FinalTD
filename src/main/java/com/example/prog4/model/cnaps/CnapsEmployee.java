c48abb4966b15df126cd7012fdbfcc0e320e192f
Fabene <hei.fabien.2@gmail.com>
2023-08-24 07:54:39 +0300

    feat: updating end_to_end_id

---
+++ src/main/java/com/example/prog4/model/cnaps/CnapsEmployee.java
@@ -0,0 +1,57 @@
+package com.example.prog4.model.cnaps;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.ColumnTransformer;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.AUTO;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+@Table(name = "\"cnapsEmployee\"")
+public class CnapsEmployee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private String id;
+    private String cin;
+    private String cnaps;
+    private String image;
+    private String address;
+    private String lastName;
+    private String firstName;
+    private String personalEmail;
+    private String professionalEmail;
+    private String registrationNumber;
+
+    private LocalDate birthDate;
+    private LocalDate entranceDate;
+    private LocalDate departureDate;
+
+    private Integer childrenNumber;
+
+    private String endToEndId;
+}
